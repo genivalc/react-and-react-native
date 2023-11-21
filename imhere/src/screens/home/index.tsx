802e13c5fff2c9eeeb282e4fe30102d9562c1124
genivalc <genivalcandeiadon.neto@gmail.com>
2023-11-21 17:56:38 -0300

    creat Home

---
+++ imhere/src/screens/home/index.tsx
@@ -0,0 +1,30 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
+
+export default function Home() {
+  function handleParticipantAdd() {
+    console.log("chamou");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento neto</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant />
+      <Participant />
+    </View>
+  );
+}
