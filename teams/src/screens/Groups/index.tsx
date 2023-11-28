2678bf03b9f1cc0f3bc3c79c0399c48c8b2aee73
genivalc <genivalcandeiadon.neto@gmail.com>
2023-11-28 18:07:19 -0300

    Add GroupCard

--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <Highlight subtitle="neto" title="genival" />
+      <GroupCard title="Galera do ignate" />
     </Container>
   );
 }
