4200ac4540f42e7a7fefcd6c6f889fb23385c560
genivalc <genivalcandeiadon.neto@gmail.com>
2023-12-04 11:39:14 -0300

    feat new group in App.tsx

--- teams/App.tsx
+++ teams/App.tsx
@@ -8,17 +8,20 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { StatusBar } from "react-native";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Groups /> : <Loading />}
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <NewGroup /> : <Loading />}
+      </ThemeProvider>
+    </>
   );
 }
