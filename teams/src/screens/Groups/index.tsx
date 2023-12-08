2240e55a56f9382405d42fd8f2a6a2e0d9fb6e02
genivalc <genivalcandeiadon.neto@gmail.com>
2023-12-08 17:07:59 -0300

    fix screens Groups Players

--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -1,15 +1,21 @@
-import { Highlight } from "@components/Highlight";
-import { Container } from "./styles";
-import { Header } from "@components/Header";
-import { GroupCard } from "@components/GroupCard";
-import { useCallback, useState } from "react";
-import { FlatList } from "react-native";
-import { ListEmpty } from "@components/ListEmpty";
-import { Button } from "@components/Button";
-import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { useState, useCallback } from "react";
+import { Alert, FlatList } from "react-native";
+
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+
 import { groupsGetAll } from "@storage/group/groupsGetAll";
 
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
+
+import { Container } from "./styles";
+import { Loading } from "@components/Loading";
+
 export function Groups() {
+  const [isLoading, setIsLoading] = useState(true);
   const [groups, setGroups] = useState<string[]>([]);
 
   const navigation = useNavigation();
@@ -20,10 +26,14 @@ export function Groups() {
 
   async function fetchGroups() {
     try {
+      setIsLoading(true);
       const data = await groupsGetAll();
       setGroups(data);
     } catch (error) {
+      Alert.alert("Turmas", "Não foi possível carregar as turmas");
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -40,18 +50,23 @@ export function Groups() {
   return (
     <Container>
       <Header />
-      <Highlight subtitle="neto" title="genival" />
-      <FlatList
-        data={groups}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
-        )}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => (
-          <ListEmpty message="Que tal cadastrar a primeira turma?" />
-        )}
-      />
+      <Highlight title="Turmas" subtitle="jogue com sua turma" />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+          )}
+          contentContainerStyle={groups.length === 0 && { flex: 1 }}
+          ListEmptyComponent={() => (
+            <ListEmpty message="Que tal cadastrar a primeira turma?" />
+          )}
+        />
+      )}
+
       <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
