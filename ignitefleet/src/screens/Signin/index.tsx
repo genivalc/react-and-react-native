06dc512e60ee9e8ed84c7019952a811c222634c6
genivalc <genivalcandeiadon.neto@gmail.com>
2024-02-29 14:28:30 -0300

    fetuare componente button ?

--- ignitefleet/src/screens/Signin/index.tsx
+++ ignitefleet/src/screens/Signin/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
-import { styles } from "./styles";
+import { Container, Slogan, Title } from "./styles";
+import backgroundImg from "../../../src/assets/background.png";
+import { Button } from "../../components/Button";
 
 export function SignI() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+
+      <Slogan>Gestão de uso de veículos</Slogan>
+      <Button title="Entrar com Google" />
+    </Container>
   );
 }
