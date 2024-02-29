ddca5bab3b21e5b12a3a1090e08cc5256c435f4c
genivalc <genivalcandeiadon.neto@gmail.com>
2024-02-29 09:59:29 -0300

    feature Fontes personalizadas + Componente de Loading

--- ignitefleet/App.tsx
+++ ignitefleet/App.tsx
@@ -1,10 +1,31 @@
 import { SignI } from "./src/screens/Signin";
 import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
+import { Loading } from "./src/components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <SignI />
     </ThemeProvider>
   );
