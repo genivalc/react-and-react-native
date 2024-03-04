5cb440b19091172e39498178dcd3bfc96803ab19
genivalc <genivalcandeiadon.neto@gmail.com>
2024-03-04 12:05:21 -0300

    add header

--- ignitefleet/src/screens/Home/index.tsx
+++ ignitefleet/src/screens/Home/index.tsx
@@ -1,5 +1,10 @@
 import { Container } from "./styles";
+import { HomeHeader } from "../../components/HomeHeader";
 
 export function Home() {
-  return <Container />;
+  return (
+    <Container>
+      <HomeHeader />
+    </Container>
+  );
 }
