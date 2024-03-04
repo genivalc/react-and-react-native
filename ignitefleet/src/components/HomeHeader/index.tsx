f5586021e9242a71144bde0831d0984a3be7e4db
genivalc <genivalcandeiadon.neto@gmail.com>
2024-03-04 14:17:59 -0300

    add Imagem do usuário

--- ignitefleet/src/components/HomeHeader/index.tsx
+++ ignitefleet/src/components/HomeHeader/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Container, Greeting, Message, Name } from "./styles";
+import { Container, Greeting, Message, Name, Picture } from "./styles";
 import { TouchableOpacity } from "react-native";
 import { Power } from "phosphor-react-native";
 
 export function HomeHeader() {
   return (
     <Container>
+      <Picture
+        source={{ uri: "https://github.com/genivalc.png" }}
+        placeholder="L184i9ofbHof00ayjsay~qj[ayj@"
+      />
       <Greeting>
         <Message>Olá</Message>
 
