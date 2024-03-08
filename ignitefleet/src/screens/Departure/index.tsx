9b139206c6fff1438a70dcf61244f1bd9e3cd015
genivalc <genivalcandeiadon.neto@gmail.com>
2024-03-08 16:33:35 -0300

    project react native more redux saga

--- ignitefleet/src/screens/Departure/index.tsx
+++ ignitefleet/src/screens/Departure/index.tsx
@@ -1,23 +1,56 @@
+import { useRef } from "react";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { TextAreaInput } from "../../components/TextAreaInput";
 import { Container, Content } from "./styles";
+import {
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 export function Departure() {
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log("OK!");
+  }
+  const keyboardAvoidingViewBehavior =
+    Platform.OS === "android" ? "height" : "position";
+
   return (
     <Container>
       <Header title="Saída" />
 
-      <Content>
-        <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
-        <TextAreaInput
-          label="Finalizade"
-          placeholder="Vou utilizar o veículo para..."
-        />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={keyboardAvoidingViewBehavior}
+      >
+        <ScrollView>
+          <Content>
+            <LicensePlateInput
+              label="Placa do veículo"
+              placeholder="BRA1234"
+              onSubmitEditing={() => {
+                descriptionRef.current?.focus();
+              }}
+              returnKeyType="next"
+            />
+            <TextAreaInput
+              ref={descriptionRef}
+              label="Finalizade"
+              placeholder="Vou utilizar o veículo para..."
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType="send"
+              blurOnSubmit
+            />
 
-        <Button title="Registrar Siada" />
-      </Content>
+            <Button title="Registrar Siada" onPress={handleDepartureRegister} />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
