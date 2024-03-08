318bbb994ebaa060457da8eed92348e338ec38ac
genivalc <genivalcandeiadon.neto@gmail.com>
2024-03-08 17:51:17 -0300

    Creating Button Icon

--- ignitefleet/src/screens/Arrival/index.tsx
+++ ignitefleet/src/screens/Arrival/index.tsx
@@ -11,6 +11,8 @@ import {
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { X } from "phosphor-react-native";
 
 type RouteParamProps = {
   id: string;
@@ -41,6 +43,7 @@ export function Arrival() {
         </Description>
 
         <Footer>
+          <ButtonIcon icon={X} />
           <Button title="Registrar chegada" />
         </Footer>
       </Content>
