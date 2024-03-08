1a49b81dae82d3a7ee36d7c80721c820cb2ee254
genivalc <genivalcandeiadon.neto@gmail.com>
2024-03-08 17:27:20 -0300

    Filtering the vehicle in use

--- ignitefleet/App.tsx
+++ ignitefleet/App.tsx
@@ -1,3 +1,4 @@
+import "react-native-get-random-values";
 import { SignIn } from "./src/screens/Signin";
 import { ThemeProvider } from "styled-components/native";
 import {
--- ignitefleet/src/screens/Home/index.tsx
+++ ignitefleet/src/screens/Home/index.tsx
@@ -2,19 +2,45 @@ import { Container, Content } from "./styles";
 import { HomeHeader } from "../../components/HomeHeader";
 import { CarStatus } from "../../components/CarStatus";
 import { useNavigation } from "@react-navigation/native";
+import { useQuery } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
 
 export function Home() {
   const { navigate } = useNavigation();
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+  const historic = useQuery(Historic);
 
   function handleRegisterMoviment() {
     navigate("departure");
   }
 
+  function fetchVehicle() {
+    try {
+      const vehicle = historic.filtered("status='departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      Alert.alert(
+        "Veículo em uso",
+        "Não foi possível carregar o veículo em uso."
+      );
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
+
   return (
     <Container>
       <HomeHeader />
       <Content>
-        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMoviment} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMoviment}
+        />
       </Content>
     </Container>
   );
