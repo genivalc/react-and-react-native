0f107c1460f8335ebf1a752b88cced856d532ec7
genivalc <genivalcandeiadon.neto@gmail.com>
2024-03-08 17:13:41 -0300

    Registering vehicle exit

--- ignitefleet/src/screens/Departure/index.tsx
+++ ignitefleet/src/screens/Departure/index.tsx
@@ -12,29 +12,62 @@ import {
   Platform,
   Alert,
 } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useUser } from "@realm/react";
+
+import { useRealm } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
 
 export function Departure() {
   const [description, setDescription] = useState("");
   const [licensePlate, setLicensePlate] = useState("");
+  const [isRegistering, setIsResgistering] = useState(false);
+
+  const realm = useRealm();
+  const user = useUser();
+  const { goBack } = useNavigation();
 
   const descriptionRef = useRef<TextInput>(null);
   const licensePlateRef = useRef<TextInput>(null);
 
   function handleDepartureRegister() {
-    if (!licensePlateValidate(licensePlate)) {
-      licensePlateRef.current?.focus();
-      return Alert.alert(
-        "Placa inválida",
-        "A placa é inválida. Por favor, informa a placa correta."
-      );
-    }
+    setIsResgistering(true);
+    try {
+      if (!licensePlateValidate(licensePlate)) {
+        licensePlateRef.current?.focus();
+        return Alert.alert(
+          "Placa inválida",
+          "A placa é inválida. Por favor, informa a placa correta."
+        );
+      }
 
-    if (description.trim().length === 0) {
-      descriptionRef.current?.focus();
-      return Alert.alert(
-        "Finalidade",
-        "Por favor, informe a finalidade da utilização do veículo"
-      );
+      if (description.trim().length === 0) {
+        descriptionRef.current?.focus();
+        return Alert.alert(
+          "Finalidade",
+          "Por favor, informe a finalidade da utilização do veículo"
+        );
+      }
+      setIsResgistering(false);
+
+      realm.write(() => {
+        realm.create(
+          "Historic",
+          Historic.generate({
+            user_id: user!.id,
+            license_plate: licensePlate,
+            description,
+          })
+        );
+      });
+
+      Alert.alert("Saída", "Saída do veículo registrada com sucesso.");
+
+      goBack();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não possível registrar a saída do veículo.");
+      setIsResgistering(false);
     }
   }
   const keyboardAvoidingViewBehavior =
@@ -70,7 +103,11 @@ export function Departure() {
               onChangeText={setDescription}
             />
 
-            <Button title="Registrar Siada" onPress={handleDepartureRegister} />
+            <Button
+              title="Registrar Siada"
+              onPress={handleDepartureRegister}
+              isLoading={isRegistering}
+            />
           </Content>
         </ScrollView>
       </KeyboardAvoidingView>
