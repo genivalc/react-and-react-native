7a3037a2b01c7b79bb624720e4781e581fdf6242
genivalc <genivalcandeiadon.neto@gmail.com>
2024-03-14 13:49:02 -0300

    tensorflow-rn

---
+++ tensorflow-rn/components/Classification/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#292728",
+    borderRadius: 9,
+    flexDirection: "row",
+    overflow: "hidden",
+  },
+  probability: {
+    backgroundColor: "#621AC4",
+    padding: 12,
+    color: "#FFF",
+  },
+  className: {
+    padding: 12,
+    color: "#FFF",
+  },
+});
