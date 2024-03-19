a749403722fdaaa564a4fb6234739791dfa0a763
gcdnascimento <gcdnascimento.accenture@rd.com>
2024-03-19 16:43:54 -0300

    fix styles quotatuinItems

--- OneBitCoin/src/components/QuotationsList/QuotatuinsItems/styles.ts
+++ OneBitCoin/src/components/QuotationsList/QuotatuinsItems/styles.ts
@@ -4,7 +4,7 @@ export const styles = StyleSheet.create({
   mainContent: {
    width: "95%",
    height: "auto",
-   backgroundColor: "#fff",
+   backgroundColor: "#111",
    marginLeft: "3%",
    marginBottom: 15,
    borderRadius: 10,
@@ -12,8 +12,34 @@ export const styles = StyleSheet.create({
    alignItems: "center",
    padding: 10
   },
+  logoBitcoin: {
+    width: 40,
+    height: 40,
+    marginLeft:2 
+  },
+  boxLogo:{
+    flexDirection: "row",
+    alignItems: "center"
+  },
   contextLeft: {
-    
-  }
+    width: "36%",
+    alignItems: "flex-start"
+  },
+  dayCotation: {
+    fontSize: 16,
+    paddingLeft: 2,
+    color: "#ddd9ce",
+    fontWeight: "bold"
+  },
+  contextRight: {
+    width: "60%",
+    alignItems: "flex-end"
+  },
+  price: {
+    fontSize: 18,
+    color: "#ddd9ce",
+    fontWeight: "bold"
+  },
+
  
 });
