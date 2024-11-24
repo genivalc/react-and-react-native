b1e035c633545d31f49adfe30eb70042855a4ea7
Genival Neto <genivalcandeiadon.neto@gmail.com>
2024-11-23 21:39:30 -0300

    Delete esports directory

--- esports/src/components/Loading/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-
-import { styles } from "./styles";
-
-export function Loading() {
-  return <View style={styles.container}></View>;
-}
